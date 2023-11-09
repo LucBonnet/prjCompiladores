4e6cb6a823e2243d887cad1e16daebae4ce972fa
LucBonnet <lucbonnet10@gmail.com>
2023-11-09 12:45:39 -0300

    bat

---
+++ prjCompiladores/dist/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+public static  double  soma ( int  a , int  b ){return  a + b ;
+}
+public static void main (String args[]){
+if( soma ( 10 , 20 ) ==  30 ){for( int  i  = (int) 0 ;
+ i  <  10 ; i  =  i + 1 ){System.out.print( i + " " );} int  i  = (int) 10 ;
+if( i  >=  10 ){System.out.println( "Maior que 10" );}else {System.out.println( "Menor que 10" );} int  valores [] = { 2 , 4 , 5 , 6 , 8 , 10 , 12 , 14 , 16 , 18 };
+while( i  !=  0 ){ i  = (int) i - 1 ;
+System.out.println( valores [ i ]);}}
+}
+}
